48c211a4b66428b24c0937c885a5643ea0b00253
idl99 <ihandilnath2012@gmail.com>
2017-12-19 00:46:06 +0530

    LoginForm FXML Layout, LoginForm controller and Login service binding

--- src/Login/LoginFormController.java
+++ src/Login/LoginFormController.java
@@ -1,10 +1,63 @@
 package Login;
 
+import Services.LoginService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import org.bson.Document;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginFormController implements javafx.fxml.Initializable{
+public class LoginFormController implements javafx.fxml.Initializable {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label lblEmployeeId;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private ImageView iconError;
+
+    @FXML
+    private Label lblErrorMsg;
+
+    @FXML
+    private TextField txtFieldEmployeeId;
+    public String getEmployeeId(){
+        return txtFieldEmployeeId.getText();
+    }
+
+    @FXML
+    private PasswordField txtFieldPassword;
+    public String getPassword(){
+        return txtFieldPassword.getText();
+    }
+
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    @FXML
+    void login(ActionEvent event) {
+        try{
+            Document authResult = LoginService.authenticate(getEmployeeId(),getPassword());
+            lblErrorMsg.setText("Login Successful");
+            lblErrorMsg.setVisible(true);
+        } catch (Exception e){
+            iconError.setVisible(true);
+            lblErrorMsg.setVisible(true);
+        }
+    }
+
 }
