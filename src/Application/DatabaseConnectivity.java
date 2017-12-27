5ac3a57be60599bf1dab6a6c31e2bb66894ae9ce
idl99 <ihandilnath2012@gmail.com>
2017-12-27 15:16:55 +0530

    Modified comments in DatabaseConnectivity.java interface

--- src/Application/DatabaseConnectivity.java
+++ src/Application/DatabaseConnectivity.java
@@ -1,6 +1,7 @@
 package Application;
 
 import Entities.BuildingFloor;
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -8,17 +9,22 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
+
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public interface DatabaseConnectivity {
-    // Interface to define the constants holding configuration to the local database.
-    // By doing so, the application code with relation to database connectivity configurations
-    // can be easily maintained through this interface.
+
+    // Interface for configurations of the database persistence layer
+    // By implementing this interface in persisted POJOs, database configurations can be easily modified
+    // and maintained through this interface
+
+    MongoClient client = new MongoClient("Localhost", 27017);
+
+    // Codec that serializes a POJO, as writable to MongoDB
     CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
             fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
-    MongoClient client = new MongoClient("Localhost", 27017);
     MongoDatabase database = client.getDatabase("Cw03Database").withCodecRegistry(pojoCodecRegistry);
 
     MongoCollection<Document> EmployeesCollection = database.getCollection("Employees");
