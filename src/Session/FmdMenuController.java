d1659db72117432584dad9b5ded0d8f1b5fedb69
idl99 <ihandilnath2012@gmail.com>
2018-01-05 21:12:49 +0530

    Added View Building Floor plans button to FMD menu

--- src/Session/FmdMenuController.java
+++ src/Session/FmdMenuController.java
@@ -3,12 +3,22 @@ package Session;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class FmdMenuController {
+public class FmdMenuController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+
     @FXML
     void recordFloorDetails(ActionEvent event) {
         newFormWindow("/Forms/FloorForm.fxml");
@@ -40,8 +50,8 @@ public class FmdMenuController {
     }
 
     @FXML
-    void initialize() {
-
+    void viewFloorPlans(ActionEvent event){
+        newFormWindow("");
     }
 
     void newFormWindow(String fxmlResource){
