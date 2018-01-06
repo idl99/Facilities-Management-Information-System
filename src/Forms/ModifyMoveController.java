a3af4fbdf0c25e3a670c0df5d00025a5f7c8fd0c
idl99 <ihandilnath2012@gmail.com>
2018-01-07 04:09:23 +0530

    - Completed Normal Staff Functionality - ModifyMove

--- src/Forms/ModifyMoveController.java
+++ src/Forms/ModifyMoveController.java
@@ -1,12 +1,18 @@
 package Forms;
 
 import Entities.MoveRequest;
+import Entities.RequestStatus;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class ModifyMoveController implements Initializable{
@@ -22,6 +28,12 @@ public class ModifyMoveController implements Initializable{
     @FXML
     private TextField txtFieldStatus;
 
+    @FXML
+    private TextField txtFieldScheduleDate;
+
+    @FXML
+    private TextField txtFieldScheduledTime;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -31,12 +43,50 @@ public class ModifyMoveController implements Initializable{
         requestToModify = MoveRequest.getRequestById(txtFieldRequestNumber.getText());
 
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        txtFieldDate.setText(MoveRequest.dateToZonedDateTime(requestToModify.getRequestedDateTime()).format(dateFormatter));
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
 
+        txtFieldDate.setText(MoveRequest.dateToZonedDateTime(requestToModify.getRequestedDateTime()).format(dateFormatter));
         txtFieldStatus.setText(requestToModify.getStatus().toString());
+        txtFieldScheduleDate.setText(MoveRequest.dateToZonedDateTime(requestToModify.getScheduledDateTime()).format(dateFormatter));
+        txtFieldScheduledTime.setText(MoveRequest.dateToZonedDateTime(requestToModify.getScheduledDateTime()).format(timeFormatter));
 
     }
 
-//    TODO Updating details of Move Request
+    @FXML void updateMoveRequest(){
+        if(requestToModify.getRequestedDateTime().getTime() !=
+                getDate().toEpochSecond()){
+            long currentTime = new Date().getTime()/(3600*1000);
+            long requestedTime = getDate().toEpochSecond()/3600;
+            long scheduledTime = requestToModify.getScheduledDateTime().getTime()/(3600*1000);
+            double timeDiffCurrentAndScheduled = scheduledTime - currentTime;
+            double timeDiffCurrentAndRequested = requestedTime - scheduledTime;
+
+            if(timeDiffCurrentAndRequested>24 && timeDiffCurrentAndScheduled>24 ){
+                requestToModify.setRequestedDateTime(MoveRequest.zonedDateTimeToDate(getDate()));
+                requestToModify.setStatus(RequestStatus.Pending);
+                requestToModify.setScheduledDateTime(new Date());
+                requestToModify.updateInDatabase();
+            }
+
+        }
+    }
+
+    @FXML
+    void cancelMoveRequest(){
+        requestToModify.setStatus(RequestStatus.Cancelled);
+        requestToModify.updateInDatabase();
+    }
+
+    @FXML
+    ZonedDateTime getDate(){
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        return ZonedDateTime.of(
+                LocalDate.parse(txtFieldDate.getText(),formatter),
+                LocalTime.of(0,0),
+                ZoneId.systemDefault()
+        );
+    }
 
 }
