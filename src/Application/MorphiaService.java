04057ad6f9d1350c69e566d809cb002f632f3254
idl99 <ihandilnath2012@gmail.com>
2018-01-07 18:54:17 +0530

    - Deployed to Cloud MongoDB server, and made subsequent changes in MorphiaService

--- src/Application/MorphiaService.java
+++ src/Application/MorphiaService.java
@@ -1,6 +1,7 @@
 package Application;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
@@ -12,7 +13,8 @@ public class MorphiaService {
     MorphiaService(){
 
         this.morphia = new Morphia();
-        this.datastore = morphia.createDatastore(new MongoClient("Localhost",27017),
+        MongoClient client = new MongoClient(new MongoClientURI("mongodb+srv://ihan:ihan@cluster0-atuo4.mongodb.net/test"));
+        this.datastore = morphia.createDatastore(client,
                 "Cw03Database");
 
     }
